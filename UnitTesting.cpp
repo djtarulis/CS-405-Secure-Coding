f8632ef3e134edb10c0f44af71a0dcff0c34adcc
Daniel <104313625+djtarulis@users.noreply.github.com>
2024-04-28 23:51:02 -0500

    Add files via upload

---
+++ UnitTesting.cpp
@@ -0,0 +1,86 @@
+// Uncomment the next line to use precompiled headers
+#include "pch.h"
+// uncomment the next line if you do not use precompiled headers
+//#include "gtest/gtest.h"
+//
+// the global test environment setup and tear down
+// you should not need to change anything here
+class Environment : public ::testing::Environment
+{
+public:
+    ~Environment() override {}
+
+    // Override this to define how to set up the environment.
+    void SetUp() override
+    {
+        //  initialize random seed
+        srand(time(nullptr));
+    }
+
+    // Override this to define how to tear down the environment.
+    void TearDown() override {}
+};
+
+// create our test class to house shared data between tests
+// you should not need to change anything here
+class CollectionTest : public ::testing::Test
+{
+protected:
+    // create a smart point to hold our collection
+    std::unique_ptr<std::vector<int>> collection;
+
+    void SetUp() override
+    { // create a new collection to be used in the test
+        collection.reset(new std::vector<int>);
+    }
+
+    void TearDown() override
+    { //  erase all elements in the collection, if any remain
+        collection->clear();
+        // free the pointer
+        collection.reset(nullptr);
+    }
+
+    // helper function to add random values from 0 to 99 count times to the collection
+    void add_entries(int count)
+    {
+        assert(count > 0);
+        for (auto i = 0; i < count; ++i)
+            collection->push_back(rand() % 100);
+    }
+};
+
+// When should you use the EXPECT_xxx or ASSERT_xxx macros?
+// Use ASSERT when failure should terminate processing, such as the reason for the test case.
+// Use EXPECT when failure should notify, but processing should continue
+
+// Test that a collection is empty when created.
+// Prior to calling this (and all other TEST_F defined methods),
+//  CollectionTest::StartUp is called.
+// Following this method (and all other TEST_F defined methods),
+//  CollectionTest::TearDown is called
+TEST_F(CollectionTest, CollectionSmartPointerIsNotNull)
+{
+    // is the collection created
+    ASSERT_TRUE(collection);
+
+    // if empty, the size must be 0
+    ASSERT_NE(collection.get(), nullptr);
+}
+
+// Test that a collection is empty when created.
+TEST_F(CollectionTest, IsEmptyOnCreate)
+{
+    // is the collection empty?
+    ASSERT_TRUE(collection->empty());
+
+    // if empty, the size must be 0
+    ASSERT_EQ(collection->size(), 0);
+}
+
+TEST_F(CollectionTest, StringNotNullPointer)
+{
+    const char* tmpPtrVal = std::getenv("TMP"); // The results from the call to std::getenv() are checked for null before the std::string object is constructed.
+    std::string tmp(tmpPtrVal ? tmpPtrVal : "");    // Construct string object
+    ASSERT_TRUE(!tmp.empty());  // Assert string object is not empty
+}
